fd407e79af896c4342e6bde166fa97c16635736d
Brett V. Forsgren <brett.forsgren@outlook.com>
2021-09-10 20:50:49 -0600

    add windows terminal settings

--- install.ps1
+++ install.ps1
@@ -1,5 +1,33 @@
 #!/usr/bin/pwsh
 
-# write .gitconfig redirect
-$thisPath = $PSScriptRoot -replace "\\","/"
-"[include]`n    path = $thisPath/.gitconfig" | Out-File -FilePath "~/.gitconfig"
+function Main() {
+    # write .gitconfig redirect
+    $thisPath = $PSScriptRoot -replace "\\", "/"
+    "[include]`n    path = $thisPath/.gitconfig" | Out-File -FilePath "~/.gitconfig"
+
+    # Windows-specific
+    if ($IsWindows) {
+        # symlink Windows Terminal settings
+        EnsureFileSymlink -sourceDirectory "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -linkName "settings.json" -destinationLocation "$PSScriptRoot\WindowsTerminal\settings.json"
+    }
+}
+
+function EnsureFileSymlink([string]$sourceDirectory, [string] $linkName, [string] $destinationLocation) {
+    Push-Location $sourceDirectory
+
+    try {
+        if (Test-Path $linkName) {
+            Remove-Item $linkName
+        }
+
+        cmd /c mklink $linkName $destinationLocation
+        if ($LASTEXITCODE -ne 0) {
+            exit $LASTEXITCODE
+        }
+    }
+    finally {
+        Pop-Location
+    }
+}
+
+Main
